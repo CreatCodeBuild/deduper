e1dccbba60f5c0bc2d7c8a0cc07294e001c355cb
CreatCodeBuild <8919794+CreatCodeBuild@users.noreply.github.com>
2023-01-24 23:33:20 +0800

    wip

--- dedup.ts
+++ dedup.ts
@@ -1,52 +1,58 @@
+// deno-lint-ignore-file no-namespace
+import { join } from "https://deno.land/std@0.173.0/path/mod.ts";
+import { wrap } from "./iter.ts";
 interface file {
-    path: string
-    name: string
+    path: string    // the absolute path of this file
+    name: string    // the base name of this file
     size: number
 }
 
-async function walkDir(rootDir: string) {
-    const dir = Deno.readDir(rootDir)
-    let paths: Array<file> = []
+namespace Duplication {
+    export interface Data {
+        name: string
+        size: number
+        paths: string[]
+    }
+    
+    export function size(dup: Data) {
+        return dup.size * dup.paths.length - 1
+    }
+}
+
+
+async function* walkDir(rootDir: string): AsyncGenerator<file> {
+    const dir = await Deno.readDir(rootDir)
     try {
         for await (const p of dir) {
-            const path = [rootDir, p.name].join(`\\`)
+            const path = join(rootDir, p.name)
             if(p.isDirectory) {
-                const subPaths = await walkDir(path)
-                paths = paths.concat(subPaths)
+                yield * walkDir(path)
             }
             if(p.isFile) {
-                const f = await Deno.open(path)
-                paths.push({
+                const fileInfo = Deno.lstatSync(path)
+                yield {
                     path: path,
                     name: p.name,
-                    size: (await f.stat()).size
-                })
+                    size: fileInfo.size
+                }
             }
         }
-    
     } catch (e) {
         console.log(e)
     }
-    return paths
-}
-interface dup {
-    name: string
-    size: number
-    paths: string[]
 }
 
-function dupSize(dup: dup) {
-    return dup.size * dup.paths.length - 1
-}
 
-function dupsToRemove(dup: dup) {
+
+
+function dupsToRemove(dup: Duplication.Data) {
     return {
         keep: dup.paths[0],
         remove: dup.paths.slice(1)
     }
 }
 
-function* findDup(paths: file[]) {
+function* findDup(fileInfos: file[]) {
 
     function sameSize(files: file[]) {
         for(const f of files) {
@@ -57,13 +63,12 @@ function* findDup(paths: file[]) {
         return true
     }
 
-    console.log("total files", paths.length)
-    const set = new Map<string, file[]>()
-    for(const p of paths) {
-        if(set.has(p.name)) {
-            set.get(p.name)?.push(p)
+    const set = new Map<string, file[]>()   // file name -> [info]
+    for(const info of fileInfos) {
+        if(set.has(info.name)) {
+            set.get(info.name)?.push(info)
         } else {
-            set.set(p.name, [p])
+            set.set(info.name, [info])
         }
     }
     console.log("files with unique name", set.size)
@@ -79,23 +84,40 @@ function* findDup(paths: file[]) {
 }
 
 
-const dPaths = await walkDir(`D:\\`)
-const ePaths = await walkDir(`E:\\`)
-const dups = Array.from(findDup(dPaths.concat(ePaths)))
+const rootDir = "/home/xiang"
+const skipDir = "WeChat Files"
+const suffix = [".jpg", ".jpeg", ".mp4", ".png", ".m4v", ".mov", ".flv"]
 
-console.log("dups", dups.length)
-// console.log(JSON.stringify(dups, null, '  '))
-for(const dup of dups) {
-    console.log(dupSize(dup))
-    console.log(...dup.paths)
-}
 
-function dupsToDelete(dups: dup[]) {
+console.log(`Start to scan ${rootDir}`)
+console.log(`Skipping ${suffix.join(" ")}`)
+
+const wrapper = wrap(walkDir(rootDir))
+const startTime = new Date()
+const totalFiles = await wrapper.finalize()
+// console.log(`Time cost ${(new Date()).getTime() - startTime.getTime()} mili sec`)
+
+const filePaths = totalFiles.filter(fileInfo => !fileInfo.path.includes(skipDir))
+const dups = Array.from(findDup(filePaths)).filter(dup=>(
+    suffix.filter(suf => dup.name.includes(suf)).length > 0
+))
+const dupSize = dups.reduce((sum, dup) => sum + dup.size * dup.paths.length, 0)
+
+console.log(`Scanned ${totalFiles.length} files`)
+console.log(`Skipped ${totalFiles.length - filePaths.length} files`)
+console.log(`There are ${dups.length} duplicated files, ${dupSize / 1024 / 1024} MBs`)
+// console.table(dups.map(dup=>({
+//     size: dupSize(dup),
+//     count: dup.paths.length,
+//     paths: JSON.stringify(dup.paths)
+// })))
+
+function dupsToDelete(dups: Duplication.Data[]) {
     return dups.map(dupsToRemove)
 }
 
-const total = dups.map(dupSize).reduce((pre,cur) => pre+cur,0)
-console.log(total, JSON.stringify(dupsToDelete(dups)))
+// const total = dups.map(dupSize).reduce((pre,cur) => pre+cur,0)
+// console.log(total, JSON.stringify(dupsToDelete(dups)))
 
 // for(const filesToDelete of dupsToDelete(dups)) {
 //     console.log("keeping", filesToDelete.keep)
@@ -105,3 +127,4 @@ console.log(total, JSON.stringify(dupsToDelete(dups)))
 //     }
 //     console.log()
 // }
+console.log(`Time cost ${(new Date()).getTime() - startTime.getTime()} mili sec`)
---
+++ makefile
@@ -0,0 +1,2 @@
+run:
+	deno run --allow-read dedup.ts
